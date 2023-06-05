f82c0061bedad4f646dfa2aab99f33d347fdcc1b
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-06-05 20:58:46 +0800

    Finalize visual styling

---
+++ src/_data/tokens.js
@@ -0,0 +1,43 @@
+module.exports = {
+  color: {
+    white: '#FFFFFF',
+    grey25: '#19182B40',
+    paleNavy: '#36384D',
+    darkNavy: '#242742',
+    vermellion: '#FF6155',
+  },
+  font: {
+    base: 'Roboto, sans-serif',
+  },
+  text: {
+    body: 16,
+    bodySmall: 12,
+    headingMin: 40,
+    headingMax: 56,
+  },
+  weight: {
+    base: 400,
+    bold: 700,
+  },
+  leading: {
+    loose: 1.5,
+    tight: 1,
+  },
+  measure: {
+    base: '60ch',
+  },
+  size: {
+    '2xs': 8,
+    xs: 12,
+    s: 16,
+    m: 24,
+    l: 36,
+    xl: 40,
+    '2xl': 48,
+    '3xl': 64,
+  },
+  viewport: {
+    min: 320,
+    max: 1440,
+  },
+}
