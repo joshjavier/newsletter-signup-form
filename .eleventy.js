ecaf2de05884f1d11b7a6a3010ecbc379c383b74
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-06-06 09:36:31 +0800

    Use 11ty HTML Base Plugin to fix absolute URLs

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy')
+
 module.exports = (config) => {
   config.addPassthroughCopy('src/fonts')
   config.addPassthroughCopy('src/images')
   config.addPassthroughCopy('src/index.js')
+
+  // Use 11ty HTML Base Plugin to fix absolute URLs in deployment
+  config.addPlugin(EleventyHtmlBasePlugin)
 
   return {
     dir: {
